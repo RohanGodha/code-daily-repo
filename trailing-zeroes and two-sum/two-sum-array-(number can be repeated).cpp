a563ae05034e943a414fea004162bc9d397ca523
RohanGodha <68965506+RohanGodha@users.noreply.github.com>
2022-07-02 00:55:42 +0530

    Create two-sum-array-(number can be repeated).cpp

---
+++ trailing-zeroes and two-sum/two-sum-array-(number can be repeated).cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int arr[5]={2,3,1,5,4},sum,a=0,b=0;
+    cout<<"Enter Sum Required: ";
+    cin>>sum;
+    for(int i=0;i<sizeof(arr);i++)
+    {
+        a=arr[i];
+        for (int j=0;j<sizeof(arr);j++)
+        {
+            b=arr[j];
+            int add=a+b;
+            if(add==sum)
+            {
+                cout<<"Pair is:("<<a<<","<<b<<")"<<endl;
+            }
+        }
+    }
+    return 0;
+}
