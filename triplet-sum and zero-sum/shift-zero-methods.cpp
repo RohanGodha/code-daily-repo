46af8bb27d14ba46d556de5669b12e5167780366
Rohan Godha <yoyorodha@gmail.com>
2022-07-06 17:12:41 +0530

    Update and rename shift-zero-method-1.cpp to shift-zero-methods.cpp

--- triplet-sum and zero-sum/shift-zero-methods.cpp
+++ triplet-sum and zero-sum/shift-zero-methods.cpp
@@ -1,4 +1,6 @@
 // Online C++ compiler to run C++ program online
+//Method 1: Copy Elements from new array where all elements are sorted if 0 or not
+//Method 2: <Below One> New Array is formed for only elements being non-zero and items are replaced.
 #include <iostream>
 using namespace std;
 int main()
