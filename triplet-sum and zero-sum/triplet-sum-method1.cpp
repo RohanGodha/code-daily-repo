6bbbfd26839c5eebb8a3e84f1a8818af1100168e
Rohan Godha <yoyorodha@gmail.com>
2022-07-06 12:02:24 +0530

    Create triplet-sum-method1.cpp

---
+++ triplet-sum and zero-sum/triplet-sum-method1.cpp
@@ -0,0 +1,32 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout<<"Triplet Sum- Method 1\n";
+    int n;
+    cout<<"Size of array: ";
+    cin>>n;
+    int arr[n],key;
+    for(int i=0;i<n;i++)
+    {
+        cout<<"\n"<<i+1<<": ";
+        cin>>arr[i];
+    }
+    cout<<"Sum to find: ";
+    cin>>key;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            for(int k=0;k<n;k++)
+            {
+               if((arr[i]+arr[j]+arr[k])==key)
+               {
+                   cout<<"\n\nThe indexes and number are: \nIndex "<<i<<" : "<<arr[i]<<"\nIndex "<<j<<" : "<<arr[j]<<"\nIndex "<<k<<" : "<<arr[k];
+               }
+            }
+        }
+    }
+    return 0;
+}
