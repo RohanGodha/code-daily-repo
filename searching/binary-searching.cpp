5e90393aa4a864dd6b5e6dda3023c47cd2b17be5
Rohan Godha <yoyorodha@gmail.com>
2022-07-07 23:37:32 +0530

    Update binary-searching.cpp

--- searching/binary-searching.cpp
+++ searching/binary-searching.cpp
@@ -1,34 +1,27 @@
 // Online C++ compiler to run C++ program online
 #include <iostream>
 using namespace std;
-int main()
+int main() {
+int arr[10]={22,33,44,55,66,77,88,99,100,110},min=0,max=sizeof(arr)-1,mid,key;
+cout<<"Enter the Number to find: ";
+cin>>key;
+int begin=arr[min],end=arr[max];
+while (begin<=end)
 {
-    int num,arr[10]={1,2,3,4,5,6,7,8,9,10},min=0,max=sizeof(arr);
-    cout<<"Number to be searched is: ";
-    cin>>num;
-    int begin=arr[min],end=arr[max], mid=arr[max/2];
-    while(mid!=num)
+    mid=min+(max-min)/2;
+    if(arr[mid]<key)
+    min=mid+1;
+    else if(arr[mid]>key)
+    max=mid-1;
+    else if(arr[mid]==key)
     {
-        if(num<mid)
-        {
-            max/=2;
-            mid=arr[(max-min)/2];
-        }
-        else if(num>mid)
-        {
-            min=max/2;
-            mid=arr[(max-min)/2];
-        }
-        else if(num==mid)
-        {
-            cout<<"Number found at index: "<<(max-min)/2;
-        }
-        else
-        {
-        cout<<"Not Existing in this array.";    
-        }
-        
+    cout<<"\nFound at Index: "<<mid;
+    break;
     }
-    cout<<"Number found at index: "<<(max-min)/2;
-    return 0;
+    else
+    {
+        cout<<"\nNot Found";
+    }
+}
+return 0;
 }
