9438a4109ed7ee5abecadcd172ae5f55347dfb5a
Rohan Godha <yoyorodha@gmail.com>
2023-06-04 06:19:42 +0530

    Create 20. Valid Parentheses.cpp

---
+++ Stacks and Queue/20. Valid Parentheses.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isValid(string s) 
+    {
+        stack<char> st;
+        for(auto c: s)
+        {
+            if(st.empty()) st.push(c);
+            else if((st.top()=='(' && c==')') || 
+                    (st.top()=='[' && c==']')|| 
+                    (st.top()=='{' && c=='}'))
+                    {
+                        st.pop();
+                    }
+            else
+            {
+                st.push(c);
+            }
+        }
+        return (st.empty());
+    }
+};
