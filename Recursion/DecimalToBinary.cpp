a02f5ce482fb8f1a153d6a1b86ae1c0024b3a048
Rohan Godha <yoyorodha@gmail.com>
2023-06-17 16:12:53 +0530

    Create DecimalToBinary.cpp

---
+++ Recursion/DecimalToBinary.cpp
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+void toBinary(int N)
+{
+    if(N==0) return;
+    toBinary(N/2);
+    cout<<N%2;
+}
+
+//{ Driver Code Starts.
+
+int main() {
+	//code
+	
+	int t;
+	cin>>t;
+	
+	
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    
+	    toBinary(n);
+	    cout<<endl;
+	}
+	return 0;
+}
+// } Driver Code Ends
