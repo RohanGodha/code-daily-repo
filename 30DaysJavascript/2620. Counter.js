9275f848b0f4b2b19b3c76e79977baea559b61b2
Rohan Godha <yoyorodha@gmail.com>
2023-07-01 22:56:25 +0530

    Create 2620. Counter.js

---
+++ 30DaysJavascript/2620. Counter.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+var createCounter = function(n) {
+    return function() 
+    {
+        // n=n+1;
+        return n++;    
+    };
+};
+
+const counter = createCounter(10)
+counter(10) // 10
+counter(11) // 11
+counter(12) // 12
