98487c3714c912ada85ea4f153ef0ca69365652b
Rohan Godha <yoyorodha@gmail.com>
2023-07-12 00:01:56 +0530

    Create 189. Rotate Array.cpp

---
+++ leetCode and GFG/189. Rotate Array.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    void rotate(vector<int>& arr, int k) {
+    int n = arr.size();
+    k = k % n; 
+    reverse(arr.begin(), arr.end());
+    reverse(arr.begin(), arr.begin() + k);
+    reverse(arr.begin() + k, arr.end());    
+    }
+};
