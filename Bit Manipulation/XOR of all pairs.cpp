093a83996b082c9a1a9b8392580c723b743a6b7c
Rohan Godha <yoyorodha@gmail.com>
2023-09-28 23:07:11 +0530

    Create XOR of all pairs.cpp

---
+++ Bit Manipulation/XOR of all pairs.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int arr[n];
+    
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int result = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            result ^= (arr[i] ^ arr[j]);
+        }
+    }
+
+    cout << "XOR of all pairs: " << result << endl;
+    
+    return 0;
+}
